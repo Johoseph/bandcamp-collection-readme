4e80068e6cb8806857f48cf2641c10fe1a353a52
Joseph Moroney <49534136+Johoseph@users.noreply.github.com>
2025-09-22 23:26:45 +1000

    Use vercel express to resolve build (#5)
    * index{.js -> .ts} for vercel
    
    * let vercel infer builds
    
    * Back to js in build + tsx for dev
    
    * force build command
    
    * + remove builds
    
    * Missing yarn release
    
    * Missing outputDirectory
    
    * fn?
    
    * version
    
    * runtime again
    
    * full version
    
    * Attempt to let vercel do everything
    
    * idk why it builds now lol
    
    * Test removing start script entirely
    
    * Remove copyfiles and let's call it a day

--- index.ts
+++ index.ts
@@ -3,8 +3,8 @@ import cors from "cors";
 import log from "npmlog";
 import path from "path";
 
-import { router } from "./router.ts";
-import { initHandlebars } from "./handlebars.ts";
+import { router } from "./router.js";
+import { initHandlebars } from "./handlebars.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 initHandlebars();
 
 app.set("view engine", "hbs");
-app.set("views", path.resolve() + "/views");
+app.set("views", path.resolve() + "/public");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -32,3 +32,5 @@ app.listen(port, () => {
     `Bandcamp collection scraper started at http://localhost:${port}`
   );
 });
+
+export default app;
